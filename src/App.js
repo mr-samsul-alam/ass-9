01e8eed027aa0728bd0fd893ca6dc1ae243db83a
mr-samsul-alam <contactsamsulalam@gmail.com>
2021-10-04 11:25:51 +0600

    second push

--- src/App.js
+++ src/App.js
@@ -1,41 +1,63 @@
 import './App.css';
-import Footer from './componants/Footer/Footer';
-import Header from './componants/Header/Header';
+ 
+ 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
-import Home from './componants/Home/Home';
-import About from './componants/About/About';
+ 
+ 
+ 
+ 
+ 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Founder from './componants/Founders/Founder';
 import NotFound from './componants/NotFound/NotFound';
-
+import Footer from './componants/Footer/Footer';
+import Service from './componants/Service/Service';
+import Home from './componants/Home/Home';
+import Header from './componants/Header/Header';
+import About from './componants/About/About';
+ 
 function App() {
   return (
     <div className="App">
-       <Router>
-       <Header></Header>
-         <Switch>
-           <Route exact path='/home'>
-          <Home></Home>
-           </Route>
-           <Route exact path='/'>
-          <Home></Home>
-           </Route>
-           <Route exact path='/about'>
-            <About></About>
-           </Route>
-           <Route exact  path='*'>
-              <NotFound></NotFound>
-           </Route>
-         </Switch>
-         <Footer></Footer>
-       </Router>
-     
-    
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/home'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/about/:id'>
+          <About></About>
+          </Route>
+          <Route exact path='/about'>
+           <About></About>
+          </Route>
+          <Route exact path='/service'>
+            <Service></Service>
+          </Route>
+          <Route exact path='/founders'>
+            <Founder></Founder>
+          </Route>
+          <Route exact path='*'>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
+ 
+ 
     </div>
   );
 }
-
+ 
 export default App;
+ 
+ 
+ 
+
--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -1,23 +1,69 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
+ 
 const Header = () => {
+ 
+ 
     return (
-        <div>
-            <h1>this is Header</h1>
-           <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/tott">Topics</Link>
-          </li>
-        </ul>
+ 
+ 
+ 
+        <div className="container">
+ 
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Our Coaching Academy</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+ 
+                            <NavLink to="/home" activeStyle={{
+                                fontWeight: "bold",
+                                color: "red"
+                            }}>Home</NavLink>
+                            <NavLink to="/about"
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "red"
+                                }}>About</NavLink>
+                            <NavLink to="/service"
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "red"
+                                }}>Service</NavLink>
+                            <NavLink to="/founders"
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "red"
+                                }}>Founders</NavLink>
+ 
+ 
+ 
+ 
+                        </Nav>
+                        <Nav>
+ 
+ 
+ 
+                        </Nav>
+                    </Navbar.Collapse>
+ 
+                </Container>
+            </Navbar>
         </div>
+ 
+ 
+ 
+ 
     );
 };
-
+ 
 export default Header;
+ 
+ 
+ 
+ 
+ 
+ 
+

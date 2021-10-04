9d92413ec80c6b1093cbbb7c52a7abdb26ce67ff
mr-samsul-alam <contactsamsulalam@gmail.com>
2021-10-04 14:21:16 +0600

    carosle update

--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -9,30 +9,45 @@ const Header = () => {
  
  
  
-        <div className="container">
+        <div>
  
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Our Coaching Academy</Navbar.Brand>
+                    <Navbar.Brand className="p-2" href="#home">Explore Knowledge</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
  
-                            <NavLink to="/home" activeStyle={{
+                            <NavLink to="/home" className="p-2  " 
+                            style={{
+                                textDecoration: 'none'
+                            }}activeStyle={{
                                 fontWeight: "bold",
                                 color: "red"
                             }}>Home</NavLink>
                             <NavLink to="/about"
+                            className="p-2  "
+                            style={{
+                                textDecoration: 'none'
+                            }}
                                 activeStyle={{
                                     fontWeight: "bold",
                                     color: "red"
                                 }}>About</NavLink>
                             <NavLink to="/service"
+                            className="p-2  "
+                            style={{
+                                textDecoration: 'none'
+                            }}
                                 activeStyle={{
                                     fontWeight: "bold",
                                     color: "red"
                                 }}>Service</NavLink>
                             <NavLink to="/founders"
+                            className="p-2  "
+                            style={{
+                                textDecoration: 'none'
+                            }}
                                 activeStyle={{
                                     fontWeight: "bold",
                                     color: "red"
--- src/componants/Home/Home.js
+++ src/componants/Home/Home.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { Button, Row } from 'react-bootstrap';
 import { Carousel } from 'react-bootstrap';
+import Cards from '../Cards/Cards';
+import { Link } from "react-router-dom";
+
 
 const Home = () => {
+    const [courses, setCourses] = useState([])
+    useEffect(() => {
+        fetch('/course_data_api.json')
+            .then(res => res.json())
+            .then(data => setCourses(data.slice(0, 4)))
+    }, [])
     return (
         <div>
-            <Carousel>
+            <Carousel fade interval={1500}>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
-                        src="https://rigorousthemescom-ebizon.netdna-ssl.com/blog/wp-content/uploads/2020/08/screely-1597058704114-1.png"
+                        className="d-block mx-auto"
+                        src="/coaching.jpg"
                         alt="First slide"
+                        width="1500px"
+                        height="800px"
+                         
                     />
                     <Carousel.Caption>
                         <h3>First slide label</h3>
@@ -18,9 +32,11 @@ const Home = () => {
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
-                        src="https://digital-photography-school.com/wp-content/uploads/2021/05/colorful-landscape-photography-1002.jpg"
-                        alt="Second slide"
+                        className="d-block mx-auto"
+                        src="/confused.jpg"
+                        alt="second slide"
+                        width="1500px"
+                        height="800px"
                     />
 
                     <Carousel.Caption>
@@ -28,8 +44,38 @@ const Home = () => {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-    
+                <Carousel.Item>
+                    <img
+                        className="d-block mx-auto"
+                        src="/learn.jpg"
+                        alt="third slide"
+                        width="1500px"
+                        height="800px"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+
             </Carousel>
+            <h1>Our Courses</h1>
+            <div className="container">
+
+                <Row xs={3} md={4} className="g-4 mt-4">
+                    {
+                        courses.map(course => <Cards courses={course}>
+                        </Cards>)
+                    }
+                </Row>
+            </div >
+            <Link to="/service">
+                <Button variant="warning" >
+                     See more
+                </Button>
+            </Link>
         </div>
     );
 };
